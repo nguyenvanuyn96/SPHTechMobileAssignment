5b0a5c7c930c847204b57653d7ca8bc4066e2f4d
Uy Nguyen <uy.nguyen@tiki.vn>
2020-07-29 23:25:26 +0700

    Init project

---
+++ SPHTechMobile-Assignment/SPHTechMobile-Assignment/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SPHTechMobile-Assignment
+//
+//  Created by Uy Tikier on 7/29/20.
+//  Copyright © 2020 Uy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.white
+        self.navigationItem.title = "SPH-Assignment"
+    }
+
+
+}
+
---
+++ SPHTechMobile-Assignment/SPHTechMobile-AssignmentTests/SPHTechMobile_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPHTechMobile_AssignmentTests.swift
+//  SPHTechMobile-AssignmentTests
+//
+//  Created by Uy Tikier on 7/29/20.
+//  Copyright © 2020 Uy Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import SPHTechMobile_Assignment
+
+class SPHTechMobile_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
